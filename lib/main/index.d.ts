9ef0c3ac15344fad06332676869c464f215ef69d
peterkarrer <peter@karrer.pro>
2017-03-07 10:41:54 +0100

    updated library

--- lib/main/index.d.ts
+++ lib/main/index.d.ts
@@ -1,8 +1,14 @@
+export interface LocationProvider {
+    location: string;
+}
 export interface Router {
-    init(): void;
+    init(locationProvider: LocationProvider): void;
+}
+export declare class HashLocationProvider {
+    location: string;
 }
 export declare function routeMethod(path: string): Function;
 export declare function routeClass(path?: string): Function;
-export declare function routeParam(target: any, key: string, index: number): void;
+export declare function routeParam(paramName: string): (target: any, methodName: string, index: number) => void;
 export declare function route(route: string): (...args: any[]) => any;
 export declare const router: Router;
